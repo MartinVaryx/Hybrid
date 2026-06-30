3821f5efbacfdc488098ff3c5142b27173886329
Varyx <martin.krc44@gmail.com>
2026-06-30 23:55:52 +0200

    Add files via upload

--- sim/script.js
+++ sim/script.js
@@ -2680,6 +2680,7 @@
                     tempImg.onload = () => {
                         tableFloor.style.setProperty('--bg-image', `url('${newUrl}')`);
                         tableFloor.classList.add('fade-in');
+                        tableFloor.classList.toggle('safari-repaint-trigger');
                         proceedWithPhase(); // Now proceed with narrative UI layout or action checks
                     };
                     tempImg.onerror = () => {
